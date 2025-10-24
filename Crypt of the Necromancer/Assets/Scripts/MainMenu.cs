4104806b5b4ddfbc46d8da690f0ff05a8b8ebd79
Jonah Uhrich <j77uhrich@gmail.com>
2025-10-24 13:18:21 -0700

    More menu work

--- Crypt of the Necromancer/Assets/Scripts/MainMenu.cs
+++ Crypt of the Necromancer/Assets/Scripts/MainMenu.cs
@@ -10,12 +10,16 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
+        ClearStats();
+
         SceneManager.LoadScene("Tutorial-Test"); // SceneManager.GetActiveScene().buildIndex + 1
         
     }
 
     public void QuitGame()
     {
+        ClearStats();
+
         #if UNITY_EDITOR
         // If in Unity Editor, stop Play Mode
         EditorApplication.isPlaying = false;
@@ -23,5 +27,12 @@ public class MainMenu : MonoBehaviour
         Debug.Log("Quit");  // Message for knowing qit button(s) work in editor
         Application.Quit();
         #endif
+    }
+
+    // On game restart/replay, stats will be set to default value
+    private void ClearStats()
+    {
+        if (GameManager.Instance != null)
+            GameManager.Instance.ClearSave();
     }
 }
